a974f62a3d59cb65568580b74dd8b537adf89279
mo-saleh-77 <mo.saleh.ed@gmail.com>
2024-01-23 21:39:00 +0200

    made changes on app

---
+++ src/components/Todo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare,faTrash,faSquareCheck, faSquare } from '@fortawesome/free-solid-svg-icons'
+
+
+
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+            {
+                task.completed ? (
+                    <FontAwesomeIcon className="complete-icon" icon={faSquareCheck} onClick={() => toggleComplete(task.id)} />
+                ) : (
+                    <FontAwesomeIcon className="complete-icon" icon={faSquare} onClick={() => toggleComplete(task.id)} />
+                )
+            }
+            <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+            <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        </div>
+    </div>
+  )
+}
